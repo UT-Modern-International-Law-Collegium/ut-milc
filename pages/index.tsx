179f83e18045821cfe14f2571804d37536fae838
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-10 02:26:45 +0900

    fix: index.tsx styling #6

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import { Box, Heading, Stack, VStack } from '@chakra-ui/react';
+import { Box, Heading, Stack, VStack, Text } from '@chakra-ui/react';
 import type { GetStaticProps, NextPage } from 'next';
 import Firstview from '../components/top/Firstview';
 import { axiosInstance } from '../lib/axios';
-import { Article } from '../lib/type';
 
 type TopPageProps = {
   data: any /* TODO: define props type */;
@@ -14,18 +13,27 @@ const TopPage: NextPage<TopPageProps> = ({ data }) => {
       {/* ファーストビュー */}
       <Firstview />
       {/* ファーストビュー以下 */}
-      <Stack pl={200}>
-        <Stack>
-          <Heading>About</Heading>
+      <Stack pl={500} pt={'100vh'}>
+        <Stack minH={300}>
+          <Heading fontFamily={'serif'} letterSpacing={2} size={'2xl'}>
+            About
+          </Heading>
+          <Text></Text>
         </Stack>
-        <Stack>
-          <Heading>News</Heading>
+        <Stack minH={300}>
+          <Heading fontFamily={'serif'} letterSpacing={2} size={'2xl'}>
+            News
+          </Heading>
         </Stack>
-        <Stack>
-          <Heading>Join us</Heading>
+        <Stack minH={300}>
+          <Heading fontFamily={'serif'} letterSpacing={2} size={'2xl'}>
+            Join us
+          </Heading>
         </Stack>
-        <Stack>
-          <Heading>Contact</Heading>
+        <Stack minH={300}>
+          <Heading fontFamily={'serif'} letterSpacing={2} size={'2xl'}>
+            Contact
+          </Heading>
         </Stack>
       </Stack>
     </Stack>
