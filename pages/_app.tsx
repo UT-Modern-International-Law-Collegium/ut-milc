713eccd487a7ba7845830a38144c03ba99ae81f5
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-10 17:04:28 +0900

    delete: chakraTheme.ts, fix: _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../components/Layout';
-import { chakraTheme } from '../lib/chakraTheme'; /* NOTE: 効いていない。 */
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider theme={chakraTheme}>
+    <ChakraProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
