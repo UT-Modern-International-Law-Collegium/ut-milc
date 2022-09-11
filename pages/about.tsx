1c03e532cd1ce053f765165f40cd22715ec3dc57
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-11 20:06:30 +0900

    fix: about.tsx size #29

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { GetStaticProps, NextPage } from 'next';
-import { Divider, Heading, HStack, Stack, Text } from '@chakra-ui/react';
+import {
+  Divider,
+  Heading,
+  HStack,
+  Stack,
+  Text,
+  useMediaQuery,
+} from '@chakra-ui/react';
 import PageTitle from '../components/utils/PageTitle';
 import { axiosInstance } from '../lib/axios';
 import { AboutPageData } from '../lib/type';
@@ -8,13 +15,23 @@ import { AboutPageData } from '../lib/type';
 type AboutPageProps = { data: AboutPageData[] };
 
 const AboutPage: NextPage<AboutPageProps> = ({ data }) => {
+  const [isLargerThan768px] = useMediaQuery('(min-width:768px)');
+
   return (
     <Stack>
-      <Stack pl={400} spacing={10}>
+      <Stack
+        pl={isLargerThan768px ? 400 : 10}
+        pr={isLargerThan768px ? 0 : 10}
+        spacing={10}
+      >
         <PageTitle minW={200}>団体紹介</PageTitle>
         {data.map((item) => {
           return (
-            <Stack key={item.id} maxW={850} w={'82%'}>
+            <Stack
+              key={item.id}
+              maxW={850}
+              w={isLargerThan768px ? '82%' : '100%'}
+            >
               <Heading>{item.title}</Heading>
               <HStack spacing={4} borderLeft={'1px solid #ccc'} pl={5}>
                 <Text>{item.content}</Text>
