322f21e364ab64d5468de3d23941b921748d5363
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-11 20:04:30 +0900

    fix: PageTitle.tsx size #29

--- components/utils/PageTitle.tsx
+++ components/utils/PageTitle.tsx
@@ -1,5 +1,11 @@
 import React, { FC } from 'react';
-import { Stack, Divider, Heading, HeadingProps } from '@chakra-ui/react';
+import {
+  Stack,
+  Divider,
+  Heading,
+  HeadingProps,
+  useMediaQuery,
+} from '@chakra-ui/react';
 
 type PageTitleProps = {
   children: React.ReactNode;
@@ -9,19 +15,32 @@ const PageTitle: FC<PageTitleProps & HeadingProps> = ({
   children,
   ...rest
 }) => {
+  const [isLargerThan768px] = useMediaQuery('(min-width:768px)');
+
   return (
     <Stack
       direction={'row'}
       alignItems={'center'}
-      pt={100}
-      w={'80%'}
-      spacing={8}
+      justifyContent={'space-between'}
+      pt={isLargerThan768px ? 100 : 20}
+      w={isLargerThan768px ? '80%' : '100%'}
+      spacing={isLargerThan768px ? 8 : 0}
     >
-      <Divider orientation={'horizontal'} opacity={1} borderColor={'#000'} />
-      <Heading fontFamily={'serif'} size={'2xl'} letterSpacing={2} {...rest}>
+      <Divider
+        orientation={'horizontal'}
+        opacity={1}
+        borderColor={'#000'}
+        w={isLargerThan768px ? 230 : 10}
+      />
+      <Heading fontFamily={'serif'} size={'2xl'} letterSpacing={2}>
         {children}
       </Heading>
-      <Divider orientation={'horizontal'} opacity={1} borderColor={'#000'} />
+      <Divider
+        orientation={'horizontal'}
+        opacity={1}
+        borderColor={'#000'}
+        w={isLargerThan768px ? 230 : 10}
+      />
     </Stack>
   );
 };
