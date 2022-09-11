cc7c3f96f32cedf596a6ded9e1d52734efcb3a02
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-11 18:19:14 +0900

    fix: achivment.tsx title #26

--- pages/achivment.tsx
+++ pages/achivment.tsx
@@ -1,12 +1,19 @@
 import { Stack } from '@chakra-ui/react';
 import { GetStaticProps, NextPage } from 'next';
+import PageTitle from '../components/utils/PageTitle';
 
 type Props = {
   data: any[];
 };
 
 const AchivmentPage: NextPage<Props> = ({ data }) => {
-  return <Stack></Stack>;
+  return (
+    <Stack>
+      <Stack pl={400} spacing={10}>
+        <PageTitle minW={200}>活動実績</PageTitle>
+      </Stack>
+    </Stack>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
