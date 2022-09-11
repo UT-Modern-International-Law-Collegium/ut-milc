422e6b1027407cf8efd9ebe6fa41f6f9a9704289
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-11 22:49:52 +0900

    fix: top* remove api connection

--- pages/api/top.ts
+++ pages/api/top.ts
@@ -1,19 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { fakeData } from '../../lib/fakeData';
 import { excuteQuery } from '../../lib/mysql';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
     case 'GET':
       try {
-        if (process.env.ENV_VAR === 'development') {
-          return res.status(200).json(fakeData.top);
-        } else {
-          const response = await excuteQuery(
-            'SELECT about FROM api_toppagedata'
-          );
-          return res.status(200).json(response);
-        }
+        const response = await excuteQuery('SELECT about FROM api_toppagedata');
+        return res.status(200).json(response);
       } catch (err) {
         throw new Error(`error at /api/top :${err}`);
       }
