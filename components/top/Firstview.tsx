45eedf7a20de375f74ab2dffbab66cc6d3ed7719
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-11 18:31:57 +0900

    fix: Firstvew.tsx heading #29

--- components/top/Firstview.tsx
+++ components/top/Firstview.tsx
@@ -5,16 +5,18 @@ import {
   Stack,
   StackProps,
   Text,
+  useMediaQuery,
 } from '@chakra-ui/react';
 
 const Firstview: FC<StackProps> = ({ ...rest }) => {
+  const [isLargetThan768px] = useMediaQuery('(min-width:768px)');
   return (
     <Stack h={'100vh'} bg={'#092025'} position={'relative'} {...rest}>
       {/* 内側の円 */}
       <CircularProgress
         thickness={'0.1px'}
         value={100}
-        size={'245px'}
+        size={isLargetThan768px ? '245px' : '100px'}
         position={'absolute'}
         top={'50%'}
         left={'50%'}
@@ -29,7 +31,7 @@ const Firstview: FC<StackProps> = ({ ...rest }) => {
         top={'50%'}
         left={'50%'}
         transform={'translate(-50%,-50%)'}
-        size={'1400px'}
+        size={isLargetThan768px ? '1400px' : '554px'}
         color={'#C9C9C9'}
       />
       <Stack
@@ -38,33 +40,53 @@ const Firstview: FC<StackProps> = ({ ...rest }) => {
         left={'50%'}
         transform={'translate(-50%,-50%)'}
       >
-        <Heading
-          style={{ writingMode: 'vertical-rl' }}
-          color={'#fff'}
-          letterSpacing={36}
-          fontSize={68}
-          fontWeight={'1'}
-          fontFamily={'serif'}
-          lineHeight={1.5}
-        >
-          <Text
-            style={{
-              writingMode: 'vertical-rl',
-            }}
-            fontSize={24}
-            m={0}
-            lineHeight={1.8}
-            pt={1}
-          >
-            東京大学
-          </Text>
-          現代国際法
-          <br />
-          <Text pt={300}>研究会</Text>
-        </Heading>
+        <TopTitle />
       </Stack>
     </Stack>
   );
+};
+
+const TopTitle: FC = () => {
+  const [isLargetThan768px] = useMediaQuery('(min-width:768px)');
+  if (isLargetThan768px) {
+    return (
+      <Heading
+        style={{ writingMode: 'vertical-rl' }}
+        color={'#fff'}
+        letterSpacing={36}
+        fontSize={68}
+        fontWeight={'1'}
+        fontFamily={'serif'}
+        lineHeight={1.5}
+      >
+        <Text
+          style={{
+            writingMode: 'vertical-rl',
+          }}
+          fontSize={24}
+          m={0}
+          lineHeight={1.8}
+          pt={1}
+        >
+          東京大学
+        </Text>
+        現代国際法
+        <br />
+        <Text pt={300}>研究会</Text>
+      </Heading>
+    );
+  } else {
+    return (
+      <Heading
+        style={{ writingMode: 'vertical-rl' }}
+        color={'#fff'}
+        fontFamily={'serif'}
+        letterSpacing={30}
+      >
+        現代国際法研究会
+      </Heading>
+    );
+  }
 };
 
 export default Firstview;
