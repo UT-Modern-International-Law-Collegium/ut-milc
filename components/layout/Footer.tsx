9fa1d6ddf16968d16e355353a4cb337ea677924c
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-13 20:52:08 +0900

    add: Footer.tsx, fix: layout.tsx working #27

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from 'react';
+import { Heading, HStack, IconButton, Stack, VStack } from '@chakra-ui/react';
+import { BsInstagram, BsTwitter } from 'react-icons/bs';
+
+const Footer: FC = () => {
+  return (
+    <VStack bg={'gray.200'} minH={200} py={8}>
+      <Heading
+        size={'lg'}
+        fontWeight={'thin'}
+        fontFamily={'serif'}
+        letterSpacing={2}
+      >
+        現代国際法研究会
+      </Heading>
+      <HStack>
+        <IconButton
+          aria-label={'instagram'}
+          icon={<BsInstagram />}
+          bg={'none'}
+        />
+        <IconButton aria-label={'twitter'} icon={<BsTwitter />} bg={'none'} />
+      </HStack>
+    </VStack>
+  );
+};
+
+export default Footer;
