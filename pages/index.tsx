27efbafe3851444c0c24bc38b69b9d8e3ec90058
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-15 01:44:41 +0900

    fix: index.tsx styling #71

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React, { FC, useEffect, useState } from 'react';
 import type { GetStaticProps, NextPage } from 'next';
 import { NextRouter, useRouter } from 'next/router';
 import moment from 'moment';
-import { motion, useScroll } from 'framer-motion';
+import { useScroll } from 'framer-motion';
 import { useForm } from 'react-hook-form';
 import { IconContext } from 'react-icons/lib';
 import { BsArrowRight } from 'react-icons/bs';
@@ -21,19 +21,27 @@ import {
   Textarea,
   Button,
   Center,
-  keyframes,
   useMediaQuery,
+  Box,
+  GridItem,
+  HStack,
+  SimpleGrid,
+  UnorderedList,
+  ListItem,
+  Divider,
+  VStack,
 } from '@chakra-ui/react';
 import Firstview from '../components/top/Firstview';
 import { axiosInstance } from '../lib/axios';
-import { Article } from '../lib/type';
+import { Article, Award } from '../lib/type';
 import SectionButton from '../components/top/SectionButton';
-import NextChakraLink from '../components/utils/NextChakraLink';
 import { fakeData } from '../lib/fakeData';
 import Layout from '../components/layout/Layout';
+import Image from 'next/image';
+import { restrictStringCount } from '../utils/restrictStringCount';
 
 type TopPageProps = {
-  data: { top: any[]; news: Article[] };
+  data: { top: any[]; news: Article[]; awards: Award[] };
 };
 
 type ContactForm = {
@@ -66,7 +74,6 @@ const DesktopContent: FC<TopPageProps> = ({ data }) => {
   const router: NextRouter = useRouter();
   const [isDisplayingFirstview, setIsDisplayingFirstview] =
     useState<boolean>(true);
-  const [isLargerThan768px] = useMediaQuery('(min-width:768px)');
   const [isLargerThan1280px] =
     useMediaQuery(
       '(min-width:1280px)'
@@ -96,23 +103,89 @@ const DesktopContent: FC<TopPageProps> = ({ data }) => {
     <Stack spacing={{ base: 0, md: 4 }}>
       {/* ファーストビュー */}
       <Firstview />
-      {/* ファーストビュー以下 */}
-      <Stack spacing={{ base: 20, md: 20 }} pt={{ base: 0, md: 20 }} px={300}>
+      {/* ファーストビュー以下part1 */}
+      <Stack
+        spacing={{ base: 20, md: 28 }}
+        pt={{ base: 0, md: 20 }}
+        px={200}
+        position={'relative'}
+      >
         {/* about */}
+        <Stack spacing={20} direction={'row'}>
+          {/* 画像 */}
+          <Box>
+            <Image
+              src={
+                'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+              }
+              alt={''}
+              width={800}
+              height={800}
+            />
+          </Box>
+          {/* タイトルとテキスト */}
+          <Stack spacing={8}>
+            <Heading
+              fontFamily={'serif'}
+              letterSpacing={2}
+              size={'2xl'}
+              textAlign={'right'}
+              textShadow={'6px 4px 1px rgb(0, 255, 177, 0.6)'}
+            >
+              About us
+            </Heading>
+            <Text fontSize={18} lineHeight={2}>
+              {data.top[0].about}
+            </Text>
+            <SectionButton
+              pt={100}
+              m={'0 0 0 auto'}
+              onClick={() => router.push('/about-us')}
+            >
+              団体紹介はこちら
+            </SectionButton>
+          </Stack>
+        </Stack>
+        {/* awards */}
         <Stack spacing={8}>
-          <Heading fontFamily={'serif'} letterSpacing={2} size={'2xl'}>
-            About us
+          <Heading
+            fontFamily={'serif'}
+            letterSpacing={2}
+            size={'2xl'}
+            textShadow={'6px 4px 1px rgb(0, 255, 177, 0.6)'}
+          >
+            Awards
           </Heading>
-          <Text fontSize={18} lineHeight={2}>
-            {data.top[0].about}
-          </Text>
-          <SectionButton onClick={() => router.push('/about-us')}>
-            団体紹介はこちら
-          </SectionButton>
+          {/* コンテンツ */}
+          <SimpleGrid columns={3} spacing={10}>
+            {data.awards.map((award: Award, index) => {
+              return (
+                <Stack
+                  key={index}
+                  p={4}
+                  borderRight={index === 2 ? 'none' : '1px'}
+                >
+                  <Heading size={'md'} textAlign={'center'}>
+                    {restrictStringCount(award.name, 12)}
+                  </Heading>
+                  <UnorderedList pl={4}>
+                    {award.detail.map((item, index) => {
+                      return <ListItem key={index}>{item}</ListItem>;
+                    })}
+                  </UnorderedList>
+                </Stack>
+              );
+            })}
+          </SimpleGrid>
         </Stack>
         {/* news */}
         <Stack spacing={8}>
-          <Heading fontFamily={'serif'} letterSpacing={2} size={'2xl'}>
+          <Heading
+            fontFamily={'serif'}
+            letterSpacing={2}
+            size={'2xl'}
+            textShadow={'6px 4px 1px rgb(0, 255, 177, 0.6)'}
+          >
             News
           </Heading>
           <TableContainer>
@@ -137,12 +210,29 @@ const DesktopContent: FC<TopPageProps> = ({ data }) => {
             全て見る
           </SectionButton>
         </Stack>
+      </Stack>
+      {/* part2（join us） */}
+      <Stack bg={'#092025'} h={700} position={'relative'}>
+        {/* arrow */}
+        <Stack
+          bg={'#fff'}
+          clipPath={'polygon(0 0, 50% 38%, 100% 0)'}
+          h={200}
+          top={0}
+          zIndex={3}
+        />
         {/* join us */}
-        <Stack spacing={8}>
-          <Heading fontFamily={'serif'} letterSpacing={2} size={'2xl'}>
+        <VStack spacing={12}>
+          <Heading
+            fontFamily={'serif'}
+            letterSpacing={2}
+            size={'2xl'}
+            textAlign={'center'}
+            color={'#fff'}
+          >
             Join us
           </Heading>
-          <Text fontSize={18}>
+          <Text fontSize={18} color={'#fff'} w={'40%'} textAlign={'center'}>
             現代国際法研究会に入会を希望される方は、以下のボタンから申し込み専用ページへ進み、フォームを送信してください。
           </Text>
           <Center>
@@ -160,59 +250,59 @@ const DesktopContent: FC<TopPageProps> = ({ data }) => {
               </Button>
             </IconContext.Provider>
           </Center>
-        </Stack>
-        {/* contact */}
-        <Stack spacing={8}>
-          <Heading fontFamily={'serif'} letterSpacing={2} size={'2xl'}>
-            Contact
-          </Heading>
-          <Text fontSize={18}>
-            ※当団体に参加を希望される方は、
-            <NextChakraLink href={'/join-us'} textDecoration={'underline'}>
-              こちら
-            </NextChakraLink>
-            からご応募ください。
-          </Text>
-          <Stack sx={{ span: { color: 'red' } }}>
-            <FormLabel fontSize={18} m={0}>
-              メールアドレス<span>*</span>
-            </FormLabel>
-            <Input
-              {...register('email', {
-                required: 'メールアドレスは必須項目です。',
-                pattern: {
-                  value:
-                    /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/,
-                  message: 'メールアドレスの形式が違います。',
-                },
-              })}
-            />
-            <span>{errors.email?.message}</span>
-          </Stack>
-          <Stack sx={{ span: { color: 'red' } }}>
-            <FormLabel fontSize={18} m={0}>
-              お問い合わせ内容<span>*</span>
-            </FormLabel>
-            <Textarea
-              minH={200}
-              {...register('content', {
-                required: 'お問い合わせ内容を入力してください。',
-              })}
-            />
-            <span>{errors.content?.message}</span>
-          </Stack>
-          <Center>
-            <Button
-              w={200}
-              // m={'0 0 0 auto'}
-              bg={'blue.100'}
-              onClick={handleSubmit(doContactFormSubmit)}
-            >
-              送信する
-            </Button>
-          </Center>
-        </Stack>
+        </VStack>
       </Stack>
+      {/* contact */}
+      {/* <Stack spacing={8}>
+        <Heading fontFamily={'serif'} letterSpacing={2} size={'2xl'}>
+          Contact
+        </Heading>
+        <Text fontSize={18}>
+          ※当団体に参加を希望される方は、
+          <NextChakraLink href={'/join-us'} textDecoration={'underline'}>
+            こちら
+          </NextChakraLink>
+          からご応募ください。
+        </Text>
+        <Stack sx={{ span: { color: 'red' } }}>
+          <FormLabel fontSize={18} m={0}>
+            メールアドレス<span>*</span>
+          </FormLabel>
+          <Input
+            {...register('email', {
+              required: 'メールアドレスは必須項目です。',
+              pattern: {
+                value:
+                  /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/,
+                message: 'メールアドレスの形式が違います。',
+              },
+            })}
+          />
+          <span>{errors.email?.message}</span>
+        </Stack>
+        <Stack sx={{ span: { color: 'red' } }}>
+          <FormLabel fontSize={18} m={0}>
+            お問い合わせ内容<span>*</span>
+          </FormLabel>
+          <Textarea
+            minH={200}
+            {...register('content', {
+              required: 'お問い合わせ内容を入力してください。',
+            })}
+          />
+          <span>{errors.content?.message}</span>
+        </Stack>
+        <Center>
+          <Button
+            w={200}
+            // m={'0 0 0 auto'}
+            bg={'blue.100'}
+            onClick={handleSubmit(doContactFormSubmit)}
+          >
+            送信する
+          </Button>
+        </Center>
+      </Stack> */}
     </Stack>
   );
 };
@@ -225,6 +315,7 @@ export const getStaticProps: GetStaticProps = async () => {
           data: {
             top: fakeData.top,
             news: fakeData.news,
+            awards: fakeData.awards,
           },
         },
       };
