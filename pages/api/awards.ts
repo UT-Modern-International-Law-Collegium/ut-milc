7284fbfe0414616d4af1ceee096e2cd68078794c
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-17 23:07:17 +0900

    fix: awards.ts isPath #95

--- pages/api/awards.ts
+++ pages/api/awards.ts
@@ -4,8 +4,19 @@ import { excuteQuery } from '../../lib/mysql';
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
     case 'GET':
-      const response = await excuteQuery('SELECT * FROM awards');
-      return res.status(200).json(response);
+      try {
+        const isPath: boolean = (req.query['path'] as string) === 'true';
+        if (isPath) {
+          const response = await excuteQuery(
+            'SELECT DISTINCT year FROM awards'
+          );
+        } else {
+          const response = await excuteQuery('SELECT * FROM awards');
+          return res.status(200).json(response);
+        }
+      } catch (err) {
+        throw new Error(`error at /api/awards: ${err}`);
+      }
     default:
       throw new Error(`method can only be GET`);
   }
