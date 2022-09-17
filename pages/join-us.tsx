b7108889c97c79e6e9edb2e02d546d444c65f20c
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-17 21:34:23 +0900

    fix: join-us.tsx #93

--- pages/join-us.tsx
+++ pages/join-us.tsx
@@ -1,5 +1,4 @@
-import { Stack } from '@chakra-ui/react';
-import { NextPage } from 'next';
+import { Stack, Text, useMediaQuery } from '@chakra-ui/react';
 import { ReactElement } from 'react';
 import Layout from '../components/layout/Layout';
 import PageTitle from '../components/utils/PageTitle';
@@ -10,13 +9,32 @@ type JoinUsPage = {};
 const JoinUsPage: NextPageWithLayout<JoinUsPage> = () => {
   return (
     <Stack
-      px={{ base: 10, md: 100 }}
       pb={{ base: 100 }}
       spacing={{ base: 10 }}
       w={{ base: '100%', md: '80%' }}
       mx={'auto'}
+      px={{ base: 0, md: 100 }}
     >
-      <PageTitle>入会申し込み</PageTitle>
+      <PageTitle px={{ base: 10, md: 0 }}>入会申し込み</PageTitle>
+      <Text fontSize={18} lineHeight={2}>
+        現代国際法研究会に入会を希望される方は、以下のフォームを入力の上送信してください。
+        <br />
+        ※フォームの表示・入力にはECCSのグーグルアカウントでログインする必要があります。
+      </Text>
+      <Stack
+        bg={'purple.50'}
+        px={{ base: 2, md: 0 }}
+        sx={{
+          iframe: {
+            w: '100%',
+            h: '100vh',
+          },
+        }}
+      >
+        <iframe src={process.env.NEXT_PUBLIC_GOOGLE_FORM_URL}>
+          読み込んでいます…
+        </iframe>
+      </Stack>
     </Stack>
   );
 };
