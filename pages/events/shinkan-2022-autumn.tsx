c0ab80ef484df451bb6c02ca9b6bd4787ff9933c
niwatoro <girlwithskyhairpin@gmail.com>
2022-09-23 14:08:21 +0900

    shinkan

--- pages/events/shinkan-2022-autumn.tsx
+++ pages/events/shinkan-2022-autumn.tsx
@@ -1,8 +1,13 @@
-import React, { ReactElement } from "react"
+import React, { isValidElement, ReactElement, useState } from "react"
 import { GetStaticProps } from "next"
 import {
+    chakra,
     Box,
     Center,
+    VStack,
+    Text,
+    keyframes,
+    usePrefersReducedMotion,
 } from "@chakra-ui/react"
 import { NextPageWithLayout } from "../_app"
 import Layout from "../../components/layout/Layout"
@@ -12,24 +17,98 @@ import { Event } from "../../lib/type"
 import Image from "next/image"
 import tsuka from "../../lib/images/tsuka.png"
 import sara from "../../lib/images/sara.png"
-import { motion } from "framer-motion"
+import { motion, isValidMotionProp } from "framer-motion"
+import {
+    GiScales
+} from "react-icons/gi"
+import {
+    FaBalanceScale,
+    FaBalanceScaleLeft,
+    FaBalanceScaleRight,
+} from "react-icons/fa"
+
+const ChakraBox = chakra(motion.div, { shouldForwardProp: isValidMotionProp })
 
 type ShinkanPage2022AutumnProps = {
     data: Event[]
 }
 
 const ShinkanPage2022Autumn: NextPageWithLayout<ShinkanPage2022AutumnProps> = ({ data }) => {
+    const [transform, setTransform] = useState(false)
+
+    return (
+        <Box bgColor="black" height="100vh" width="100xw" textAlign="center">
+            <Box marginTop="40vh" textColor="white" fontFamily="serif" zIndex={100}>
+                <Text fontSize="6xl" fontWeight="bold">世界へ挑む者よ、来たれ。</Text>
+                <Box marginTop="5vh">
+                    <Text fontSize="2xl">日程：いつ？</Text>
+                    <Text fontSize="2xl">場所：どこ？</Text>
+                </Box>
+            </Box>
+            <UTMilcIcon />
+        </Box >
+    )
+}
+
+const UTMilcIcon: React.FC = () => {
+    const icon_size = 500
+    const scale_size = 300
+    const text_string: string = "University  of  Tokyo  Modern  International  Law  College  "
+    const text_array = Array.from(text_string)
+    const text_length = text_array.length
     return (
         <Box>
-            <Center>
+            <Box marginTop="-50vh" marginLeft={"calc(50vw - " + (icon_size / 2) + "px)"} width={icon_size} height={icon_size} opacity={0.5}>
                 <motion.div
-                    animate={{ x: 100 }}>
-                    <Image src={sara} width={200} height={200} />
+                    animate={{
+                        rotate: [0, 180, 360]
+                    }}
+                    transition={{
+                        ease: "linear",
+                        duration: 20,
+                        repeat: Infinity,
+                    }}>
+                    <Box
+                        fontSize="2xl"
+                        width={icon_size}
+                        height={icon_size}
+                        textAlign="center"
+                        textColor="white">
+                        {text_array.map((elem, idx) =>
+                            <Box
+                                position="absolute"
+                                left=""
+                                width={icon_size}
+                                height={icon_size / 2}
+                                transformOrigin="center bottom"
+                                transform={"rotate(" + (idx * 360 / text_length) + "deg)"}
+                                key={idx}>{elem}</Box>)}
+                    </Box>
                 </motion.div>
-                <Image src={tsuka} width={400} height={400} />
-                <Image src={sara} width={200} height={200} />
-            </Center>
+            </Box>
+            <Box position="absolute" left={"calc(50vw - "+scale_size/2+"px)"} top={"calc(-13vh + "+scale_size+"px)"}>
+                <motion.div>
+                    <GiScales color="white" size={scale_size}  opacity={0.5}/>
+                </motion.div>
+            </Box>
         </Box>
+    )
+}
+
+const Door: React.FC<{ onClick: () => void, transform: string, children: React.ReactNode }> = ({ onClick, transform, children }) => {
+    return (
+        <Box
+            onClick={onClick}
+            top={0}
+            left={0}
+            backgroundColor="gray.600"
+            width={200}
+            height="50vh"
+            transformOrigin="left"
+            transition={"all 0.5s ease-in-out"}
+            transform={transform}>
+            {children
+            }</Box>
     )
 }
 
