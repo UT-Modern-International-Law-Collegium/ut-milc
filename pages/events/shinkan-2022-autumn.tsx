a57a643a2491af133044fc5660fa18b8e53d69ed
niwatoro <girlwithskyhairpin@gmail.com>
2022-09-23 20:24:00 +0900

    npm run buildでエラー修正

--- pages/events/shinkan-2022-autumn.tsx
+++ pages/events/shinkan-2022-autumn.tsx
@@ -59,7 +59,7 @@ const ShinkanPage2022Autumn: NextPageWithLayout<ShinkanPage2022AutumnProps> = ({
             <UTMilcIcon />
             <Box margin="50vh 100px">
                 <Center>
-                    <Text>{data[0].content}</Text>
+                    <Text>{data.content}</Text>
                 </Center>
             </Box>
         </Box >
