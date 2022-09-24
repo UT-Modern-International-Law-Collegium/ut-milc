80a5a3cf3745b812057318ac326221ce6bf3989f
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-25 00:51:34 +0900

    [FATAL]fix: api/awards/index.tsx logging #109

--- pages/api/awards/index.ts
+++ pages/api/awards/index.ts
@@ -2,14 +2,19 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { excuteQuery } from '../../../lib/mysql';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log('#####/api/awards/index.ts#####');
   switch (req.method) {
     case 'GET':
       try {
         const isPath: boolean = (req.query['path'] as string) === 'true';
         if (isPath) {
+          console.log('isPath has passed.');
           const response = await excuteQuery(
             'SELECT DISTINCT year FROM awards'
           );
+          console.log('#########################');
+          console.log({ response });
+          console.log('#########################');
           return res.status(200).json(response);
         } else {
           const response = await excuteQuery('SELECT * FROM awards');
