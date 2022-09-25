b679d89d16245fd09bd3752663938e7d72f865bf
KikuchiHaruto <o5o206felt@icloud.com>
2022-09-25 23:44:32 +0900

    fix: _app.tsx seo #53

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import React, { ReactElement, ReactNode } from 'react';
 import { NextPage } from 'next';
-import { ReactElement, ReactNode } from 'react';
 import Head from 'next/head';
+import type { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -20,6 +21,28 @@ const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
         <title>東京大学現代国際法研究会</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+      <DefaultSeo
+        defaultTitle={'東京大学現代国際法研究会'}
+        description={'東京大学現代国際法研究会のホームページです。'}
+        openGraph={{
+          type: 'website',
+          title: '東京大学現代国際法研究会',
+          description: '東京大学現代国際法研究会のホームページです。',
+          locale: 'ja_JP',
+          url: 'https://utmilc.com/',
+          site_name: '東京大学現代国際法研究会のホームページ',
+          images: [
+            {
+              // TODO: replace this image with valid one.
+              url: 'https://images.unsplash.com/photo-1589829545856-d10d557cf95f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
+              width: 500,
+              height: 500,
+              alt: '東京大学現代国際法研究会ホームページのプレビュー画像',
+              type: 'image/jpeg',
+            },
+          ],
+        }}
+      />
       <ChakraProvider>{getlayout(<Component {...pageProps} />)}</ChakraProvider>
     </>
   );
