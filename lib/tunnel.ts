719bbc443a3e317a1dc7de09e13f13813f06f23d
KikuchiHaruto <o5o206felt@icloud.com>
2022-10-02 18:51:29 +0900

    fix: mysql.js #109

--- lib/tunnel.ts
+++ lib/tunnel.ts
@@ -9,17 +9,22 @@ type Config = {
   localPort: number;
   dstHost: string;
   dstPort: number;
+  privateKey: any;
+  passphrase: string;
 };
 
 class Tunnel {
   private static connections: any[] = new Array<any>();
-  public static async createConnection(config: Config) {
+  public static async createConnection(config: Partial<Config>) {
     let tnl = tunnel(config, (err: any, tnl: any) => {
       if (err) {
         throw err;
       } else {
         this.connections.push(tnl);
       }
+    });
+    tnl.on('error', (err) => {
+      console.error(err);
     });
   }
   public static async closeAllConnections() {
