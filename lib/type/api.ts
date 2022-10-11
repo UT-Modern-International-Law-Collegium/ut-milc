4ab8b0c3e35bcfdab6899a069c8edee74c0e8291
KikuchiHaruto <o5o206felt@icloud.com>
2022-10-11 18:59:09 +0900

    fix: api.ts, about-us.tsx, about-us.ts #128

--- lib/type/api.ts
+++ lib/type/api.ts
@@ -1,5 +1,9 @@
+import { AboutUsSection, Member } from './page';
+
 export type TopRes = {
   about: string;
   award: string;
   join_us: string;
 };
+
+export type AboutRes = { sections: AboutUsSection[]; members: Member[] };
