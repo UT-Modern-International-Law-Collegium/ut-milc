150e5d42623a5d9eb44e07869e64925bee95e3ac
KikuchiHaruto <o5o206felt@icloud.com>
2022-10-11 21:43:41 +0900

    fix: axios.ts headers #128

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,4 +2,8 @@ import axios, { AxiosInstance } from 'axios';
 
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL: process.env.BASE_URL,
+  headers: {
+    'User-Agent': '*',
+    Accept: 'application/json; charset=UTF-8',
+  },
 });
