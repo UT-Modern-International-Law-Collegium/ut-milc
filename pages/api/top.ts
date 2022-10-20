dcc2d4c72775e64280a6b8355e0627481afc22f7
KikuchiHaruto <o5o206felt@icloud.com>
2022-10-20 14:26:31 +0900

    [FATAL]test: top.ts remove if #109

--- pages/api/top.ts
+++ pages/api/top.ts
@@ -7,29 +7,19 @@ const handler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
-  if (process.env.ENV_VAR === 'development') {
-    // 開発環境においてはfakeDataを返す。
-    const topRes: TopRes = {
-      about: fakeData.top.about,
-      award: fakeData.top.award,
-      join_us: fakeData.top.join_us,
-    };
-    res.status(200).json(topRes);
-  } else {
-    // 本番環境においてはdbからデータを取得。
-    if (req.method === 'GET') {
-      try {
-        const response = await excuteQuery(
-          'SELECT about, award, join_us FROM top_content ORDER BY id DESC LIMIT 1'
-        );
-        const topRes: TopRes = response[0];
-        res.status(200).json(topRes);
-      } catch (err) {
-        throw new Error(`error at /api/top :${err}`);
-      }
-    } else {
-      throw new Error(`method must be only GET at /api/top: ${req.method}`);
+  // 本番環境においてはdbからデータを取得。
+  if (req.method === 'GET') {
+    try {
+      const response = await excuteQuery(
+        'SELECT about, award, join_us FROM top_content ORDER BY id DESC LIMIT 1'
+      );
+      const topRes: TopRes = response[0];
+      res.status(200).json(topRes);
+    } catch (err) {
+      throw new Error(`error at /api/top :${err}`);
     }
+  } else {
+    throw new Error(`method must be only GET at /api/top: ${req.method}`);
   }
 };
 
