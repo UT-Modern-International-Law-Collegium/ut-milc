f053d958e70c18935c8c3f837e2d72c3abbf111c
KikuchiHaruto <o5o206felt@icloud.com>
2022-10-21 09:48:15 +0900

    fix: index.tsx remove console #109

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,8 +40,6 @@ type TopPageProps = {
 };
 
 const TopPage: NextPageWithLayout<TopPageProps> = ({ data }) => {
-  console.log({ data });
-
   const router: NextRouter = useRouter();
   const [isLargerThan768px] = useMediaQuery('(min-width:768px)');
 
