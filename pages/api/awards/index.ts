a7a720a05d20a6e871000abef79b6c383c6a95cb
KikuchiHaruto <o5o206felt@icloud.com>
2022-11-04 22:26:31 +0900

    fix: awards/index.ts

--- pages/api/awards/index.ts
+++ pages/api/awards/index.ts
@@ -1,48 +1,44 @@
+import { AxiosResponse } from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { fakeData } from '../../../lib/fakeData';
-import { excuteQuery } from '../../../lib/mysql';
+import { axiosWpInstance } from '../../../lib/axios';
 import { AwardYearsRes } from '../../../lib/type/api';
-import { Award } from '../../../lib/type/page';
+import { WpAwardRes, WpTagName } from '../../../lib/type/wp';
 
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
-  // isPathはgetStaticPathsとgetStaticPropsののどちらでapiが呼び出されているかの識別子。
-  const isPath: boolean = (req.query['path'] as string) === 'true';
-  if (process.env.ENV_VAR === 'development') {
-    if (isPath) {
-      const awards: Award[] = fakeData.awards;
-      const tmpYears: number[] = awards.map((award: Award) => award.year);
-      const years: number[] = Array.from(new Set(tmpYears));
-      let awardYearsRes: AwardYearsRes = years.map((year: number) => ({
-        year: year,
-      }));
-      res.status(200).json(awardYearsRes);
-    } else {
-      // NOTE: 使用場面がないので未実装
+  if (req.method === 'GET') {
+    try {
+      // isPathはgetStaticPathsとgetStaticPropsののどちらでapiが呼び出されているかの識別子。
+      const isPath: boolean = (req.query['path'] as string) === 'true';
+      if (isPath) {
+        const wpRes: AxiosResponse<any, any> = await axiosWpInstance.get(
+          `/posts?&categories=3&_fields=id,content,title,tags`
+        );
+        const wpResData: WpAwardRes[] = wpRes.data;
+        const tmpYears: number[] = await Promise.all(
+          wpResData.map(async (data: WpAwardRes) => {
+            const tmpWpYearRes: AxiosResponse<any, any> =
+              await axiosWpInstance.get(`/tags/${data.tags[0]}?_fields=name`);
+            const tmpWpYearResData: WpTagName = tmpWpYearRes.data;
+            return Number(tmpWpYearResData.name);
+          })
+        );
+        const years: number[] = Array.from(new Set(tmpYears));
+        const awardYearsRes: AwardYearsRes[] = years.map((year: number) => ({
+          year: year,
+        }));
+        res.status(200).json(awardYearsRes);
+      } else {
+        // NOTE: 使用場面がないので未実装
+        return res.status(200).json({});
+      }
+    } catch (err) {
+      return res.status(500).json(err);
     }
   } else {
-    switch (req.method) {
-      case 'GET':
-        try {
-          if (isPath) {
-            const awardYearsRes: AwardYearsRes = await excuteQuery(
-              'SELECT DISTINCT year FROM awards'
-            );
-            res.status(200).json(awardYearsRes);
-          } else {
-            /**  NOTE: 使用場面がないので未実装
-             * const response = await excuteQuery('SELECT * FROM awards');
-             * ↑このように全件取得するのがapi的に妥当だと思われる。
-             */
-          }
-        } catch (err) {
-          throw new Error(`error at /api/awards: ${err}`);
-        }
-      default:
-        throw new Error(`method can only be GET`);
-    }
+    return res.status(405).json({ err: 'method not allowed' });
   }
 };
 
