010f227b5bf291b10e7fbe095a07c3ee20219ce6
KikuchiHaruto <o5o206felt@icloud.com>
2022-11-13 12:22:43 +0900

    fix: _app.tsx, add: ga4.tsx #160

---
+++ lib/ga4.tsx
@@ -0,0 +1,47 @@
+import { FC, useEffect } from 'react';
+import { NextRouter, useRouter } from 'next/router';
+import Script from 'next/script';
+
+const GA4: FC = () => {
+  const router: NextRouter = useRouter();
+  const { events } = router;
+  useEffect(() => {
+    const f = (url: string): void => {
+      if (!window) {
+        return;
+      } else {
+        (window as any).gtag(
+          'config',
+          `${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`,
+          {
+            page_path: url,
+          }
+        );
+      }
+    };
+    events.on('routeChangeComplete', f);
+    events.on('hashChangeComplete', f);
+  }, [events]);
+
+  return (
+    <>
+      <Script
+        async
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+      />
+      <Script strategy="afterInteractive" id={'ga4-script'}>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+    </>
+  );
+};
+
+export default GA4;
