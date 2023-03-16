f664adf3a896f5c97000c96e3e2a895df8f27881
KikuchiHaruto <o5o206felt@icloud.com>
2023-03-16 23:39:25 +0900

    add: AwardCard.tsx, fix: index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,6 @@ import {
   BsFillCaretRightFill,
   BsCheckCircle,
   BsArrowRightShort,
-  BsAward,
 } from 'react-icons/bs';
 import {
   Heading,
@@ -25,7 +24,6 @@ import {
   HStack,
   LinkBox,
   IconButton,
-  Icon,
 } from '@chakra-ui/react';
 
 import Firstview from '../components/top/Firstview';
@@ -36,6 +34,7 @@ import NewsCard from '../components/news/NewsCard';
 import { NextPageWithLayout } from './_app';
 import Layout from '../components/layout/Layout';
 import { TopRes } from '../lib/type/api';
+import AwardCard from '../app/components/AwardCard';
 
 type Props = {
   data: { aboutUs: string; award: string; joinUs: string; news: News[] };
@@ -114,48 +113,7 @@ const Page: NextPageWithLayout<Props> = ({ data }) => {
               全ての成績を見る
             </SectionButton>
           )}
-          <VStack
-            w={{ base: 'none', md: '60%' }}
-            spacing={{ base: 2, md: 4 }}
-            py={{ base: 4, md: 8 }}
-            px={4}
-            borderRadius={8}
-            border={'solid'}
-            borderColor={'gray.300'}
-            position={'relative'}
-          >
-            <Icon
-              as={BsAward}
-              position={'absolute'}
-              top={-6}
-              left={-8}
-              color={'yellow.300'}
-              opacity={0.6}
-              w={{ base: 100, md: 140 }}
-              h={{ base: 100, md: 140 }}
-            />
-            <Heading
-              fontWeight={'light'}
-              textAlign={'center'}
-              fontSize={{ base: 22, md: 30 }}
-              zIndex={1}
-            >
-              2022 Philip C. Jessup International Law Moot Court Competition
-              National Round
-            </Heading>
-            <Text fontSize={{ base: 18, md: 20 }}>
-              ~ NationalRound（国内予選） ~
-            </Text>
-            <HStack alignItems={'baseline'}>
-              <Text fontSize={{ base: 18, md: 20 }}>総合結果</Text>
-              <Text fontSize={{ base: 24, md: 32 }} pl={{ base: 2, md: 4 }}>
-                優勝
-              </Text>
-              <Text as="span" fontSize={{ base: 18, md: 20 }}>
-                （国際大会進出）
-              </Text>
-            </HStack>
-          </VStack>
+          <AwardCard />
           {/* タイトルとテキスト */}
           <Stack
             w={{ base: '100%', md: '40%' }}
