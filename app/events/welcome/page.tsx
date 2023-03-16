68fa9c92207ea5939020d9b7983972096f838b28
KikuchiHaruto <o5o206felt@icloud.com>
2023-03-17 00:10:52 +0900

    add: layout/ fix: page.tsx, layout.tsx

--- app/events/welcome/page.tsx
+++ app/events/welcome/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { Box, Center, Heading, Spinner } from '@chakra-ui/react';
+import { Box, Center, Divider, Heading, Spinner, Text } from '@chakra-ui/react';
 
 import { prefix } from '../../../lib/prefix';
 import { WelcomeData } from '../../../lib/type/welcomeData';
@@ -20,16 +20,36 @@ const Page = () => {
 
   if (!welcome) {
     return (
-      <Center>
+      <Center minH={'100vh'}>
         <Spinner color={'teal'} h={24} w={24} mt={24} />
       </Center>
     );
   }
 
   return (
-    <Box>
-      <Heading dangerouslySetInnerHTML={{ __html: welcome.title }} />
-      <Box dangerouslySetInnerHTML={{ __html: welcome.content }} />
+    <Box w={{ lg: '64%' }} mx={'auto'} py={20}>
+      <Heading
+        dangerouslySetInnerHTML={{ __html: welcome.title }}
+        textAlign={'center'}
+        fontSize={42}
+        fontFamily={'serif'}
+      />
+      <Divider my={8} borderColor={'teal.500'} />
+      <Box>
+        <Text
+          dangerouslySetInnerHTML={{ __html: welcome.content }}
+          lineHeight={2}
+          sx={{
+            h2: { fontSize: 28, my: 4, fontWeight: 'bold' },
+            h3: { fontSize: 20, my: 4, fontWeight: 'bold' },
+            a: { color: 'teal.600', textDecoration: 'underline' },
+            img: {
+              h: 200,
+              w: 200,
+            },
+          }}
+        />
+      </Box>
     </Box>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,15 @@
 
 import { ChakraProvider } from '@chakra-ui/react';
 
+import Layout from './components/layout/Layout';
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ja">
       <body>
-        <ChakraProvider>{children}</ChakraProvider>
+        <ChakraProvider>
+          <Layout>{children}</Layout>
+        </ChakraProvider>
       </body>
     </html>
   );
