eeb404dca8ad147529fedde6f6ed9a8e7f3ea3cf
KikuchiHaruto <o5o206felt@icloud.com>
2023-03-16 23:37:16 +0900

    add: route.ts, welcomeData.ts, prefix.ts, fix: page.tsx

--- app/events/welcome/page.tsx
+++ app/events/welcome/page.tsx
@@ -1,9 +1,37 @@
 'use client';
 
-import { Box } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Box, Center, Heading, Spinner } from '@chakra-ui/react';
+
+import { prefix } from '../../../lib/prefix';
+import { WelcomeData } from '../../../lib/type/welcomeData';
 
 const Page = () => {
-  return <Box></Box>;
+  const [welcome, setWelcome] = useState<WelcomeData>();
+
+  useEffect(() => {
+    const f = async () => {
+      const res = await fetch(`${prefix()}/events/welcome`);
+      const data: { data: WelcomeData } = await res.json();
+      setWelcome(data.data);
+    };
+    f();
+  }, []);
+
+  if (!welcome) {
+    return (
+      <Center>
+        <Spinner color={'teal'} h={24} w={24} mt={24} />
+      </Center>
+    );
+  }
+
+  return (
+    <Box>
+      <Heading dangerouslySetInnerHTML={{ __html: welcome.title }} />
+      <Box dangerouslySetInnerHTML={{ __html: welcome.content }} />
+    </Box>
+  );
 };
 
 export default Page;
