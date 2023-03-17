4cd8b66f8a2aee5e88f4eb60f1974e20264bacf2
KikuchiHaruto <o5o206felt@icloud.com>
2023-03-18 01:49:51 +0900

    add: line.png, fix: page.tsx

--- app/(pages)/join-us/page.tsx
+++ app/(pages)/join-us/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { Heading, Stack, Text } from '@chakra-ui/react';
+import { Heading, Link, Stack, Text } from '@chakra-ui/react';
+import Image from 'next/image';
 
 import NextChakraLink from '../../components/utils/NextChakraLink';
 
@@ -35,33 +36,28 @@ const Page = () => {
         lineHeight={{ base: 1.5, md: 2 }}
         px={{ base: 4, md: 0 }}
       >
-        現代国際法研究会に入会を希望される方は、以下のフォームを入力の上送信してください。
-        <br />
-        ※フォームの表示・入力にはECCSのグーグルアカウントでログインする必要があります。
-        <br />
-        ※フォームが表示されない場合は、
-        <NextChakraLink
-          href={'https://forms.gle/35kzG9g1yVxsFFiy5'}
+        2023年度は公式LINEから入会フォームに回答していただく流れになっております。入会を少しでも検討されている方は公式LINEの友達追加をお願いいたします！
+      </Text>
+      <Text>
+        URL:
+        <Link
+          href="https://lin.ee/gwVPQY5"
+          target="_blank"
+          rel="noreferrer"
+          pl={2}
+          color={'teal.600'}
           textDecoration={'underline'}
         >
-          こちら
-        </NextChakraLink>
-        からフォームの入力・送信をお願いします。
+          https://lin.ee/gwVPQY5
+        </Link>
       </Text>
-      <Stack
-        bg={'purple.50'}
-        px={{ base: 2, md: 0 }}
-        sx={{
-          iframe: {
-            w: '100%',
-            h: '100vh',
-          },
-        }}
-      >
-        <iframe src={process.env.NEXT_PUBLIC_GOOGLE_FORM_URL}>
-          読み込んでいます…
-        </iframe>
-      </Stack>
+      <Text>QR</Text>
+      <Image
+        src="/line.png"
+        width={200}
+        height={200}
+        alt="東大国際法研の公式LINEQRコード"
+      />
     </Stack>
   );
 };
