b8176a05bd51e0db02ab250a6980170a8fb425a3
KikuchiHaruto <o5o206felt@icloud.com>
2023-03-18 02:12:40 +0900

    [WIP]add: awards2/[year]/page.tsx #173

---
+++ app/(pages)/awards2/[year].tsx/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { useEffect } from 'react';
+
+const Page = ({ params }: { params: { year: string } }) => {
+  const { year } = params;
+
+  useEffect(() => {
+    const f = async () => {};
+  }, []);
+};
+
+export default Page;
