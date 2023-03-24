85ec692c52a91e5580d139a7e0f90df1ec9f22d1
KikuchiHaruto <o5o206felt@icloud.com>
2023-03-24 20:55:11 +0900

    delete: some files #179

--- lib/restrictStringCount.ts
+++
@@ -1,8 +0,0 @@
-export const restrictStringCount = (value: string, count: number): string => {
-  if (value.length > count) {
-    const slicedValue = value.slice(0, count - 1);
-    return `${slicedValue}...`;
-  } else {
-    return value;
-  }
-};
