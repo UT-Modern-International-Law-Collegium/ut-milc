89bcda769f6af7cb00b037248a2e36731df87d88
KikuchiHaruto <kikuchi@amunzen.com>
2023-05-25 11:22:40 +0900

    fix: tailwind.config.js teal #196

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./app/**/*.{tsx,ts,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        teal: {
+          50: '#E6FFFA',
+          100: '#B2F5EA',
+          200: '#81E6D9',
+          300: '#4FD1C5',
+          400: '#38B2AC',
+          500: '#319795',
+          600: '#2C7A7B',
+          700: '#285E61',
+          800: '#234E52',
+          900: '#1D4044',
+        },
+      },
+    },
   },
   plugins: [],
 };
