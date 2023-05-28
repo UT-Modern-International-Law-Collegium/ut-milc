709174feb81cccab4b4068ae7fd9248e433a53c7
KikuchiHaruto <kikuchi@amunzen.com>
2023-05-28 17:46:17 +0900

    delete: Awards.tsx, fix: page.tsx #199

--- app/awards/page.tsx
+++ app/awards/page.tsx
@@ -1,20 +1,34 @@
-import { Awards } from './Awards';
+import { nextPrefix } from '@/lib/nextPrefix';
+import { AwardData } from '@/lib/type/awardData';
 import { fetchYears } from './fetchYears';
 
 const fetchData = async ({ year }: { year: string }) => {
   const years = await fetchYears();
-
   const yearID = years.find((item) => item.year === Number(year))?.id ?? null;
 
-  return yearID;
+  try {
+    const res = await fetch(`${nextPrefix()}/awards?tag_id=${yearID}`);
+    const data = await res.json();
+    return data.data;
+  } catch (_) {
+    return [];
+  }
 };
 
 const Page = async ({ searchParams }: { searchParams: { year: string } }) => {
   const { year } = searchParams;
-  const yearID = await fetchData({ year });
+  const awards: AwardData[] = await fetchData({ year });
 
-  /* @ts-expect-error Server Component */
-  return <Awards yearID={yearID} />;
+  return (
+    <div>
+      <h2>{year}年度</h2>
+      {awards.length === 0 ? (
+        <p>当該年度の活動実績はありません。</p>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 };
 
 export default Page;
