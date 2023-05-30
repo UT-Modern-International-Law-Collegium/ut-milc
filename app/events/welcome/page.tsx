4e8fb6de717f498d6e2f7a9503f565bbf8c4f625
KikuchiHaruto <kikuchi@amunzen.com>
2023-05-30 12:11:46 +0900

    add: style.css page.tsx

--- app/events/welcome/page.tsx
+++ app/events/welcome/page.tsx
@@ -1,55 +1,24 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-import { Box, Center, Divider, Heading, Spinner, Text } from '@chakra-ui/react';
+import './style.css';
 
 import { WelcomeData } from '@/lib/type/welcomeData';
+import { nextPrefix } from '@/lib/nextPrefix';
+import { InnerHTML } from '@/app/_components/InnerHTML';
 
-const Page = () => {
-  const [welcome, setWelcome] = useState<WelcomeData>();
-
-  useEffect(() => {
-    const f = async () => {
-      const res = await fetch(`/api/events/welcome`);
-      const data: { data: WelcomeData } = await res.json();
-      setWelcome(data.data);
-    };
-    f();
-  }, []);
-
-  if (!welcome) {
-    return (
-      <Center minH={'100vh'}>
-        <Spinner color={'teal'} h={24} w={24} mt={24} />
-      </Center>
-    );
-  }
+const Page = async () => {
+  const res = await fetch(`${nextPrefix()}/events/welcome`);
+  const data: { data: WelcomeData } = await res.json();
+  const welcome = data.data;
 
   return (
-    <Box w={{ lg: '64%' }} mx={'auto'} py={20} px={{ base: 5, lg: 0 }}>
-      <Heading
-        dangerouslySetInnerHTML={{ __html: welcome.title }}
-        textAlign={'center'}
-        fontSize={42}
-        fontFamily={'serif'}
+    <div className="w-11/12 md:w-[88vw] lg:w-[68vw] mx-auto md:py-24 py-20">
+      <InnerHTML
+        content={welcome.title}
+        className="text-4xl text-center font-serif"
       />
-      <Divider my={8} borderColor={'teal.500'} />
-      <Box>
-        <Text
-          dangerouslySetInnerHTML={{ __html: welcome.content }}
-          lineHeight={2}
-          sx={{
-            h2: { fontSize: 28, my: 4, fontWeight: 'bold' },
-            h3: { fontSize: 20, my: 4, fontWeight: 'bold' },
-            a: { color: 'teal.600', textDecoration: 'underline' },
-            img: {
-              h: 200,
-              w: 200,
-            },
-          }}
-        />
-      </Box>
-    </Box>
+      {/* divider */}
+      <div className="border-t my-4" />
+      <InnerHTML content={welcome.content} className="custom-text" />
+    </div>
   );
 };
 
