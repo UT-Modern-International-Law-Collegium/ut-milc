fe0e489240ccbcec1876d4150c21b4fffa9d3b8e
KikuchiHaruto <kikuchi@amunzen.com>
2023-05-31 10:35:37 +0900

    add: MobileHeader.tsx, fix: layout.tsx, delete: Header.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
-import './globals.css';
+import "./globals.css";
 
-import Header from './_components/_layout/Header';
-import Footer from './_components/_layout/Footer';
+import Footer from "./_components/_layout/Footer";
+import { DesktopHeader } from "./_components/_layout/header/DesktopHeader";
+import { MobileHeader } from "./_components/_layout/header/MobileHeader";
 
 export const metadata = {
-  title: '東京大学現代国際法研究会',
-  description: '東京大学現代国際法研究会のホームページです。',
-  themeColor: '#ffffff',
-  manifest: '/favicons/site.webmanifest',
+  title: "東京大学現代国際法研究会",
+  description: "東京大学現代国際法研究会のホームページです。",
+  themeColor: "#ffffff",
+  manifest: "/favicons/site.webmanifest",
   viewport: {
-    width: 'device-width',
+    width: "device-width",
     initialScale: 1,
     maximumScale: 1,
   },
@@ -19,7 +20,8 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ja">
       <body>
-        <Header />
+        <DesktopHeader />
+        <MobileHeader />
         <div className="min-h-screen">{children}</div>
         <Footer />
       </body>
