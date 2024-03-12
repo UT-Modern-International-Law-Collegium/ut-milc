71164a071c549c92fac5c64dbc17725304c245c3
KikuchiHaruto <kikuchi@amunzen.com>
2024-03-12 19:10:22 +0900

    Refactor image gallery and update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-console.log('#########################');
-console.log(`ENV_VAR is ${process.env.ENV_VAR}`);
-console.log('#########################');
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ["images.unsplash.com"],
   },
 };
 
